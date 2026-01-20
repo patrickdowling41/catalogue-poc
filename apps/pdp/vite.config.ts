64beb3a6715cf3dac8b56298c90a27f29fe9e1d9
pdowling <pdowling@officeworks.com.au>
2026-01-20 16:08:33 +1100

    fix: fixed vite issues with loading dynamic base

--- apps/pdp/vite.config.ts
+++ apps/pdp/vite.config.ts
@@ -1,5 +1,5 @@
 import { vitePlugin as remix } from '@remix-run/dev';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import tailwindcss from '@tailwindcss/vite';
 
@@ -9,23 +9,26 @@ declare module '@remix-run/node' {
   }
 }
 
-export default defineConfig({
-  root: __dirname,
-  plugins: [
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_singleFetch: true,
-        v3_lazyRouteDiscovery: true,
-      },
-    }),
-    tailwindcss(),
-    nxViteTsPaths(),
-  ],
-  server: {
-    port: 5174,
-  },
-  base: `${process.env.BASE_URL}:${process.env.PORT}/`,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    root: __dirname,
+    plugins: [
+      remix({
+        future: {
+          v3_fetcherPersist: true,
+          v3_relativeSplatPath: true,
+          v3_throwAbortReason: true,
+          v3_singleFetch: true,
+          v3_lazyRouteDiscovery: true,
+        },
+      }),
+      tailwindcss(),
+      nxViteTsPaths(),
+    ],
+    server: {
+      port: 5174,
+    },
+    base: `${env.BASE_URL}:${env.PORT}/`,
+  };
 });
